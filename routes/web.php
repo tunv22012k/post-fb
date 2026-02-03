4394f6a5a29ccc89cf701654db360930de00dd4b
TuNV <tunv@unitech.vn>
2026-02-03 11:06:13 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::get('/', function () {
 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/run-schedule', function () {
+    \Illuminate\Support\Facades\Artisan::call('schedule:run');
+    return 'Scheduler executed';
+});
